42b32d7df7c80e01d2614a1d8035761127c8cde9
Lucas Tanaka <71355554+IkasuAra@users.noreply.github.com>
2020-10-18 10:55:17 -0300

    adding form

---
+++ happy/public/scripts/page-orphanage.js
@@ -0,0 +1,66 @@
+
+// console.log(): testa a funcionalidade da ferramenta
+// false: desabilita
+
+const options = {
+    dragging: false,
+    touchZoom: false,
+    doubleClickZoom: false,
+    scrollwheelZoom: false,
+    zoomControl: false
+}
+
+//create map / passar argumento para outra função
+const map = L.map('mapid', options).setView([-23.5513,-46.6317356], 13)
+
+//create and add tileLayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+).addTo(map)
+
+//create icon
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
+
+//create and add marker
+L
+.marker([-23.5513,-46.6317356], { icon })
+.addTo(map)
+
+/* image gallery */
+
+function selectImage(event) {
+    const button = event.currentTarget
+
+    console.log(button.children)
+
+    //remover todas as classes active
+    const buttons = document.querySelectorAll(".images button")
+    buttons.forEach(removeActiveClass)
+
+    function removeActiveClass(button) {
+        button.classList.remove("active")
+    }
+
+    //selecionar a image clicada
+    const image = button.children[0]
+    const imageContainer = document.querySelector(".orphanage-details > img")
+
+    //atualizar o container de images
+    imageContainer.src = image.src
+
+    //adicionar a classe .active para este botão
+    button.classList.add('active')
+}
+
+
+
+
+
+
+
+
+
